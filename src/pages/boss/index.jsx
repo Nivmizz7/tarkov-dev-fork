45b570464a7e82f01375df6b1f27b232177a4a6a
Razzmatazz <razz@badkarma.xyz>
2026-03-26 12:37:23 -0500

    fix knight boss page

--- src/pages/boss/index.jsx
+++ src/pages/boss/index.jsx
@@ -431,7 +431,7 @@ function BossPage(params) {
                 </h2>
                 <ul>
                     {bossData.reports.map((report, index) => {
-                        const reportedMap = Object.values(allMaps).find((m) => m.id === report.map.id);
+                        const reportedMap = Object.values(allMaps).find((m) => m.id === report.map);
                         let relativeTime = getRelativeTimeAndUnit(new Date(parseInt(report.timestamp)).getTime());
                         return (
                             <li key={`report-${index}`}>
